633896edc20dcb9c00af4c848a633141f652ff7b
CroissantWarrior1 <272955@student.pwr.edu.pl>
2026-03-10 16:52:06 +0000

    Create project structure for application

---
+++ src/main/kotlin/pt/unl/fct/iadi/bookstore/Diai2Application.kt
@@ -0,0 +1,11 @@
+package pt.unl.fct.iadi.bookstore
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class Diai2Application
+
+fun main(args: Array<String>) {
+	runApplication<Diai2Application>(*args)
+}
